b332841af8d78a9a11e44d8c4e4ffa313d9a45dc
pc shao <shaopcbb@gmail.com>
2026-08-26 12:14:22 +0800

    Initial Commit

---
+++ LocalTranslate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalTranslate
+//
+//  Created by shaopc on 2026/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalTranslate/LocalTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalTranslateApp.swift
+//  LocalTranslate
+//
+//  Created by shaopc on 2026/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
